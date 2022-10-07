70b95e9ca1783acbf75cfd967a427b6299095b1e
Toby <toby.monk@teamgoldflame.com>
2022-10-07 20:02:07 +0200

    Added extra characteristics

--- ESP32/main.py
+++ ESP32/main.py
@@ -12,14 +12,20 @@ import random
 import struct
 
 
-_ENV_SENSE_UUID = bluetooth.UUID(0x181A)  # org.bluetooth.service.environmental_sensing
-_ENV_SENSE_TEMP_UUID = bluetooth.UUID(0x2A6E) # org.bluetooth.characteristic.temperature
-_ENV_SENSE_AWS_UUID = bluetooth.UUID(0x2A72) # org.bluetooth.characteristic true wind speed
-_ENV_SENSE_AWA_UUID = bluetooth.UUID(0x2A73) # uses apparent wind direction rather than speed - should not do this in practice!!
+_ENV_SENSE_UUID = bluetooth.UUID("90D3D000-C950-4DD6-9410-2B7AEB1DD7D8")  # custom service definition
+_ENV_SENSE_TEMP_UUID = bluetooth.UUID("90D3D001-C950-4DD6-9410-2B7AEB1DD7D8") # org.bluetooth.characteristic.temperature
+_ENV_SENSE_AWS_UUID = bluetooth.UUID("90D3D002-C950-4DD6-9410-2B7AEB1DD7D8") # org.bluetooth.characteristic true wind speed
+_ENV_SENSE_AWA_UUID = bluetooth.UUID("90D3D003-C950-4DD6-9410-2B7AEB1DD7D8") # uses apparent wind direction rather than speed - should not do this in practice!!
 
 # latitude longitude
-_ENV_SENSE_LAT_UUID = bluetooth.UUID(0x2AAE)
-_ENV_SENSE_LON_UUID = bluetooth.UUID(0x2AAF)
+_ENV_SENSE_LAT_UUID = bluetooth.UUID("90D3D005-C950-4DD6-9410-2B7AEB1DD7D8")
+_ENV_SENSE_LON_UUID = bluetooth.UUID("90D3D006-C950-4DD6-9410-2B7AEB1DD7D8")
+
+_ENV_SENSE_SPEED_UUID = bluetooth.UUID("90D3D007-C950-4DD6-9410-2B7AEB1DD7D8")
+_ENV_SENSE_MAX_SPEED_UUID = bluetooth.UUID("90D3D008-C950-4DD6-9410-2B7AEB1DD7D8")
+_ENV_SENSE_DISTANCE_UUID = bluetooth.UUID("90D3D009-C950-4DD6-9410-2B7AEB1DD7D8")
+_ENV_SENSE_HEADING_UUID = bluetooth.UUID("90D3D00A-C950-4DD6-9410-2B7AEB1DD7D8")
+_ENV_SENSE_IS_RECORDING_UUID = bluetooth.UUID("90D3D00B-C950-4DD6-9410-2B7AEB1DD7D8")
 
 # org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
@@ -33,25 +39,28 @@ device_service = aioble.Service(_ENV_SENSE_UUID)
 temp_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True)
 aws_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_AWS_UUID, read=True, notify=True)
 awa_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_AWA_UUID, read=True, notify=True)
+
 lat_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_LAT_UUID, read=True, notify=True)
 lon_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_LON_UUID, read=True, notify=True)
+
+speed_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_SPEED_UUID, read=True, notify=True)
+max_speed_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_MAX_SPEED_UUID, read=True, notify=True)
+distance_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_DISTANCE_UUID, read=True, notify=True)
+heading_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_HEADING_UUID, read=True, notify=True)
+recording_status_characteristic = aioble.Characteristic( device_service, _ENV_SENSE_IS_RECORDING_UUID, read=True, notify=True)
 
 aioble.register_services(device_service)
 
 _timer_start = time.ticks_ms()
 _connected_timer_start = time.ticks_ms()
 
-# Helper to encode the temperature characteristic encoding (sint16, hundredths of a degree).
-# Big endian , 2 bytes
-def _encode_temperature(temp_deg_c):
-    return struct.pack(">h", int(temp_deg_c * 100))
-
+# All variables big endian
 
 # This would be periodically polling a hardware sensor.
 async def temp_sensor():
     t = 24.5
     while True:
-        encoded_temp = _encode_temperature(t)
+        encoded_temp = struct.pack(">h", int(t * 100))
         print(f'Total time: {int(time.ticks_diff(time.ticks_ms(), _timer_start) / 1000)}, Connected time: {int(time.ticks_diff(time.ticks_ms(), _connected_timer_start) / 1000)}, Temperature: {t}')
         temp_characteristic.write(encoded_temp, send_update=True)
         t += random.uniform(-0.5, 0.5)
@@ -84,6 +93,40 @@ async def wind_sensor():
         await asyncio.sleep_ms(1000)
 
 
+async def speed_heading_sensor():
+    speed = 6.0
+    max_speed = 0.0
+    distance = 0.0
+    heading = 80
+
+    time_step_s = 1
+
+    while True:
+        # Send the data
+        speed_to_send = struct.pack(">h", int(speed * 100))
+        max_speed_to_send = struct.pack(">h", int(max_speed * 100))
+        distance_to_send = struct.pack(">h", int(distance * 100))
+        heading_to_send = struct.pack(">h", int(heading * 100))
+        speed_characteristic.write(speed_to_send, send_update=True)
+        max_speed_characteristic.write(max_speed_to_send, send_update=True)
+        distance_characteristic.write(distance_to_send, send_update=True)
+        heading_characteristic.write(heading_to_send, send_update=True)
+
+        #print("Sent Speed, heading, max speed and distance")
+
+        # simulate updates
+        max_speed = max([max_speed, speed])
+        distance += speed * time_step_s / 60 / 60
+        speed = max([0, speed + random.uniform(-0.5, 0.5)])
+        heading = (heading + random.uniform(-5, 5)) % 360
+        await asyncio.sleep_ms(int(time_step_s * 1000))
+
+async def notify_recording_status():
+    recording_status = False
+    while True:
+        recording_status_characteristic.write(struct.pack(">h", int(recording_status)), send_update=True)
+        await asyncio.sleep(5)
+
 # Serially wait for connections. Don't advertise while a central is
 # connected.
 async def peripheral_task():
@@ -109,13 +152,14 @@ async def main():
     print('Starting Bluetooth sensor example.')
     sensors = [asyncio.create_task(temp_sensor()),
                asyncio.create_task(location_sensor()),
-               asyncio.create_task(wind_sensor())]
+               asyncio.create_task(wind_sensor()),
+               asyncio.create_task(speed_heading_sensor()),
+               asyncio.create_task(notify_recording_status())
+               ]
 
     t2 = asyncio.create_task(peripheral_task())
     await asyncio.gather(*sensors, t2)
 
     print('Example finished.')
-
-
 
 asyncio.run(main())
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -6,20 +6,29 @@ var ConnectSourceButton = document.querySelector('#connect_button');
 
 // configure the display
 var windSpeedDisplay = document.querySelector('#wind_speed');
-var windDirectionDisplay = document.querySelector('#wind_direction');
+var windAngleDisplay = document.querySelector('#wind_angle');
 var longitudeDisplay = document.querySelector('#gps_lon');
 var latitudeDisplay = document.querySelector('#gps_lat');
 
+var speedDisplay = document.querySelector('#speed');
+var maxSpeedDisplay = document.querySelector('#max_speed');
+var distanceDisplay = document.querySelector('#distance');
+var headingDisplay = document.querySelector('#heading');
+var isRecordingDisplay = document.querySelector('#is_recording');
+
 
 // Register bluetooth data sources, connect to parsers and display elements
-registerBluetoothDataSource(BluetoothDataSources, 'environmental_sensing', 0x2A72, blehandle_sint16, windSpeedDisplay, '')
-registerBluetoothDataSource(BluetoothDataSources, 'environmental_sensing', 0x2A73, blehandle_sint16, windDirectionDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D002-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, windSpeedDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D003-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, windAngleDisplay, '')
 
-registerBluetoothDataSource(BluetoothDataSources, 'environmental_sensing', 'latitude', blehandle_double, latitudeDisplay, '')
-registerBluetoothDataSource(BluetoothDataSources, 'environmental_sensing', 'longitude', blehandle_double, longitudeDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D005-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_double, latitudeDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D006-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_double, longitudeDisplay, '')
 
-
-
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D007-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, speedDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D008-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, maxSpeedDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D009-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, distanceDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D00A-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, headingDisplay, '')
+registerBluetoothDataSource(BluetoothDataSources, "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8", "90D3D00B-C950-4DD6-9410-2B7AEB1DD7D8", blehandle_sint16, isRecordingDisplay, '')
 
 // Utility functions
 function registerBluetoothDataSource(BluetoothDataSourcesArray, BluetoothServiceUUID, BluetoothCharacteristicUUID, ValueHandler, TargetSelector, DataLog) {
@@ -49,13 +58,13 @@ function connectBlueToothCharacteristic(BluetoothDevice, BluetoothServiceUUID, B
 ConnectSourceButton.addEventListener('click', function() {
   console.log('Requesting Bluetooth Service...')
   navigator.bluetooth.requestDevice({
-    //acceptAllDevices : true,
-    filters:[{name :'ESP32'}],
-    optionalServices: ['battery_service', 'generic_access', 'environmental_sensing']
+    //acceptAllDevices : true, { services: ['battery_service'] }
+    filters:[{services :["90D3D000-C950-4DD6-9410-2B7AEB1DD7D8".toLowerCase()]}],
+    optionalServices: ['battery_service', 'generic_access', 'environmental_sensing', "90D3D000-C950-4DD6-9410-2B7AEB1DD7D8".toLowerCase()]
   })
   .then(device => {
     BluetoothDataSources.forEach(source => {
-      connectBlueToothCharacteristic(device, source.BluetoothServiceUUID, source.BluetoothCharacteristicUUID, source.ValueHandler, source.TargetSelector, source.DataLog);
+      connectBlueToothCharacteristic(device, source.BluetoothServiceUUID.toLowerCase(), source.BluetoothCharacteristicUUID.toLowerCase(), source.ValueHandler, source.TargetSelector, source.DataLog);
     })
   })
   .catch(error => {
@@ -72,14 +81,14 @@ function blehandle_sint16(event, TargetSelector, DataLog) {
 }
 
 function blehandle_sint32(event, TargetSelector, DataLog) {
-  console.log(event.target.value.byteLength)
+  //console.log(event.target.value.byteLength)
   const value = event.target.value.getInt32(0, false);
   //console.log('Received: ' + value);
   TargetSelector.textContent = String(value / 1000) ;
 }
 
 function blehandle_double(event, TargetSelector, DataLog) {
-  console.log(event.target.value.byteLength)
+  //console.log(event.target.value.byteLength)
   const value = event.target.value.getFloat64(0, false);
   //console.log('Received: ' + value);
   TargetSelector.textContent = String(value.toFixed(6)) ;
